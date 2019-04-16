b715a9b8a4ca01510a4d152e901ca8cd357e437e
Kohki Sato <you@example.com>
2019-04-16 17:53:09 +0900

    Added new SDEs

--- script/scripts.js
+++ script/scripts.js
@@ -4,32 +4,18 @@ console.log("script.js is running ")
 lpTag.section = ["auth"];
 lpTag.sdes.push(
    {
-        "type": "ctmrinfo",  //MANDATORY
-        "info": {
-           "cstatus": "テスト",  //CUSTOMER LIFECYCLE STATUS. FROM PRE-DEFINED LIST
-           "ctype": "テスト",  //CUSTOMER TYPE OR TIER. FROM PRE-DEFINED LIST
-           "customerId": "138766AC",  //UNIQUE CUSTOMER IDENTIFIER
-           "balance": -400.99,  //THE CUSTOMER FINANCIAL BALANCE IN DECIMAL VALUE
-           "currency": "USD",  //CURRENCY CODE
-           "socialId": "11256324780",  //SOCIAL ID OF YOUR CHOICE: FACEBOOK, TWITTER ETC...
-           "imei": "3543546543545688",  //UNIQUE DEVICE OR PHONE IDENTIFIER
-           "userName": "user000",  //CONSUMER NICKNAME OR USERNAME
-           "companySize": 500,  //COMPANY SIZE MEASURED BY NUMBER OF EMPLOYEES
-           "accountName": "bank corp",  //THE CUSTOMER'S COMPANY NAME
-           "role": "broker",  //CONSUMER ROLE TITLE
-           "lastPaymentDate": {
-                 "day": 15,  //THE DAY OF THE LAST PAYMENT NUMERIC VALUE
-                 "month": 10,  //THE MONTH OF THE LAST PAYMENT NUMERIC VALUE
-                 "year": 2014  //THE YEAR OF THE LAST PAYMENT NUMERIC VALUE
-           },
-           "registrationDate": {
-                 "day": 23,  //THE DAY OF THE REGISTRATION NUMERIC VALUE
-                 "month": 5,  //THE MONTH OF THE REGISTRATION NUMERIC VALUE
-                 "year": 2013  //THE YEAR OF THE REGISTRATION NUMERIC VALUE
-           },
-           "storeNumber": "123865",  //STORE NUMBER
-           "storeZipCode": "20505"  //STORE ZIP CODE
-        }
+        "type": "cart",  //MANDATORY
+        "total": 11.7,  //TOTAL VALUE OF THE CART AFTER DISCOUNT
+        "currency": "USD",  //CURRENCY CODE
+        "numItems": 6,  //NUMBER OF ITEMS IN CART
+        "products": [{
+            "product": {
+            "name": "prod1",  //PRODUCT NAME
+            "category": "category",  //PRODUCT CATEGORY NAME
+            "sku": "sku",  //PRODUCT SKU OR UNIQUE IDENTIFIER
+            "price": 7.8  //SINGLE PRODUCT PRICE
+            }, "quantity": 1  //NUMBER OF PRODUCTS
+        }]
    }
 );
 
