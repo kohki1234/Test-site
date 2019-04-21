5d84e158aedf645c1c3099652c5e05468aee468f
Kohki Sato <you@example.com>
2019-04-21 17:14:22 +0900

    Commented out unnecessary SDEs

--- script/scripts.js
+++ script/scripts.js
@@ -2,35 +2,35 @@ console.log("script.js is running ")
 
 // Pushing SDE's to LE
 lpTag.section = ["auth"];
-lpTag.sdes.push(
-   {
-        "type": "ctmrinfo",  //MANDATORY
-        "info": {
-           "cstatus": "テスト",  //CUSTOMER LIFECYCLE STATUS. FROM PRE-DEFINED LIST
-           "ctype": "テスト",  //CUSTOMER TYPE OR TIER. FROM PRE-DEFINED LIST
-           "customerId": "138766AC",  //UNIQUE CUSTOMER IDENTIFIER
-           "balance": -400.99,  //THE CUSTOMER FINANCIAL BALANCE IN DECIMAL VALUE
-           "currency": "USD",  //CURRENCY CODE
-           "socialId": "11256324780",  //SOCIAL ID OF YOUR CHOICE: FACEBOOK, TWITTER ETC...
-           "imei": "3543546543545688",  //UNIQUE DEVICE OR PHONE IDENTIFIER
-           "userName": "user000",  //CONSUMER NICKNAME OR USERNAME
-           "companySize": 500,  //COMPANY SIZE MEASURED BY NUMBER OF EMPLOYEES
-           "accountName": "bank corp",  //THE CUSTOMER'S COMPANY NAME
-           "role": "broker",  //CONSUMER ROLE TITLE
-           "lastPaymentDate": {
-                 "day": 15,  //THE DAY OF THE LAST PAYMENT NUMERIC VALUE
-                 "month": 10,  //THE MONTH OF THE LAST PAYMENT NUMERIC VALUE
-                 "year": 2014  //THE YEAR OF THE LAST PAYMENT NUMERIC VALUE
-           },
-           "registrationDate": {
-                 "day": 23,  //THE DAY OF THE REGISTRATION NUMERIC VALUE
-                 "month": 5,  //THE MONTH OF THE REGISTRATION NUMERIC VALUE
-                 "year": 2013  //THE YEAR OF THE REGISTRATION NUMERIC VALUE
-           },
-           "storeNumber": "123865",  //STORE NUMBER
-           "storeZipCode": "20505"  //STORE ZIP CODE
-        }
-   },
+// lpTag.sdes.push(
+//    {
+//         "type": "ctmrinfo",  //MANDATORY
+//         "info": {
+//            "cstatus": "テスト",  //CUSTOMER LIFECYCLE STATUS. FROM PRE-DEFINED LIST
+//            "ctype": "テスト",  //CUSTOMER TYPE OR TIER. FROM PRE-DEFINED LIST
+//            "customerId": "138766AC",  //UNIQUE CUSTOMER IDENTIFIER
+//            "balance": -400.99,  //THE CUSTOMER FINANCIAL BALANCE IN DECIMAL VALUE
+//            "currency": "USD",  //CURRENCY CODE
+//            "socialId": "11256324780",  //SOCIAL ID OF YOUR CHOICE: FACEBOOK, TWITTER ETC...
+//            "imei": "3543546543545688",  //UNIQUE DEVICE OR PHONE IDENTIFIER
+//            "userName": "user000",  //CONSUMER NICKNAME OR USERNAME
+//            "companySize": 500,  //COMPANY SIZE MEASURED BY NUMBER OF EMPLOYEES
+//            "accountName": "bank corp",  //THE CUSTOMER'S COMPANY NAME
+//            "role": "broker",  //CONSUMER ROLE TITLE
+//            "lastPaymentDate": {
+//                  "day": 15,  //THE DAY OF THE LAST PAYMENT NUMERIC VALUE
+//                  "month": 10,  //THE MONTH OF THE LAST PAYMENT NUMERIC VALUE
+//                  "year": 2014  //THE YEAR OF THE LAST PAYMENT NUMERIC VALUE
+//            },
+//            "registrationDate": {
+//                  "day": 23,  //THE DAY OF THE REGISTRATION NUMERIC VALUE
+//                  "month": 5,  //THE MONTH OF THE REGISTRATION NUMERIC VALUE
+//                  "year": 2013  //THE YEAR OF THE REGISTRATION NUMERIC VALUE
+//            },
+//            "storeNumber": "123865",  //STORE NUMBER
+//            "storeZipCode": "20505"  //STORE ZIP CODE
+//         }
+//    },
 //    {
 //       "type": "cart",  //MANDATORY
 //       "total": 11.7,  //TOTAL VALUE OF THE CART AFTER DISCOUNT
@@ -45,25 +45,25 @@ lpTag.sdes.push(
 //           }, "quantity": 1  //NUMBER OF PRODUCTS
 //       }]
 //  },
- {
-   "type": "purchase",  //MANDATORY
-   "total": 11.7,  //TOTAL VALUE OF THE TRANSACTION AFTER DISCOUNT
-   "currency": "USD",  //CURRENCY CODE
-   "orderId": "DRV1534XC",  //UNIQUE ORDER ID OR RECEIPT ID
-   "cart":{
-       "products": [{
-            "product": {
-                 "name": "antivirus pro plan",  //PRODUCT NAME
-                 "category": "software",  //PRODUCT CATEGORY NAME
-                 "sku": "xyz001",  //PRODUCT SKU OR UNIQUE IDENTIFIER
-                 "price": 7.8  //SINGLE PRODUCT PRICE
-            },
-       "quantity": 3  //QUANTITY OF THIS PRODUCT
-       }]
-   }
-}
+//  {
+//    "type": "purchase",  //MANDATORY
+//    "total": 11.7,  //TOTAL VALUE OF THE TRANSACTION AFTER DISCOUNT
+//    "currency": "USD",  //CURRENCY CODE
+//    "orderId": "DRV1534XC",  //UNIQUE ORDER ID OR RECEIPT ID
+//    "cart":{
+//        "products": [{
+//             "product": {
+//                  "name": "antivirus pro plan",  //PRODUCT NAME
+//                  "category": "software",  //PRODUCT CATEGORY NAME
+//                  "sku": "xyz001",  //PRODUCT SKU OR UNIQUE IDENTIFIER
+//                  "price": 7.8  //SINGLE PRODUCT PRICE
+//             },
+//        "quantity": 3  //QUANTITY OF THIS PRODUCT
+//        }]
+//    }
+// }
  
-);
+// );
 
 // chat authentication script 
 window.lpGetAuthenticationToken = function (callback) {
